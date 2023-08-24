fed185d3a0e2a0ff5e510995e110eb1262354761
Adelraga <Adelraga@github.com>
2023-08-24 07:09:14 +0300

    edit page

---
+++ lib/views/widgets/edit_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EditViewBody extends StatelessWidget {
+  const EditViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      
+    );
+  }
+}
